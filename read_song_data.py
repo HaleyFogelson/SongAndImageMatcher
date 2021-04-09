019b841234e6c983ba34f765c638f1b110ca2081
Dylan Robinson <Dylan Robinson>
2021-04-09 16:03:31 -0400

    script to put songs into playlist

--- read_song_data.py
+++ read_song_data.py
@@ -1,8 +1,31 @@
 import pandas as pd
 import sqlite3
 import spotipy
+import spotipy.util as util
 
 from spotipy.oauth2 import SpotifyClientCredentials
+
+
+cid =""
+secret = ""
+username = ""
+
+def getSP():
+
+
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid,
+                                                        client_secret=secret)
+
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    scope = 'playlist-modify-private playlist-modify-public playlist-read-private user-library-read'
+    token = util.prompt_for_user_token(username, scope, client_id=cid, client_secret=secret, redirect_uri='http://localhost/')
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        print("Success?")
+
+    else:
+        print("Can't get token for", username)
+    return sp
 
 
 songs_db_con = sqlite3.connect('track_metadata.db')
@@ -15,16 +38,25 @@ songs_db_con.close()
 
 song_titles = songs_df['title']
 
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+sp = getSP()
 
 song_ids = []
 
 for title in song_titles:
     query = title.replace(' ', '+')
-    search_result = spotify.search(query)
-    if len(search_result['tracks']['items']) > 0:
-        song = search_result['tracks']['items'][0]
-        song_ids.append(song['id'])
-        print(song['name'])
+    try:
+        search_result = sp.search(query)
+        if len(search_result['tracks']['items']) > 0:
+            song = search_result['tracks']['items'][0]
+            song_ids.append(song['id'])
+            print(song['name'])
+            print(len(song_ids))
+    except:
+        sp = getSP()
+    if len(song_ids) >= 9000:
+        break
+
+for i in range(0, len(song_ids), 100):
+    sp.user_playlist_add_tracks(username, '23LcX1S7f04C8homMmZ03Z', song_ids[i:i+100])
 
 print(song_ids)
