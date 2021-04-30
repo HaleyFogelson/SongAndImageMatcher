46916dbe2bd1c29ce6b9473d3b7c60638c3fa8f8
Dylan Robinson <Dylan Robinson>
2021-04-30 18:19:14 -0400

    changes

--- song_clustering.py
+++ song_clustering.py
@@ -7,10 +7,10 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, SpectralClustering
 
 cid ="38a975768416491cbf5ac70921868d46"
-secret = ""
+secret = "54105fa53c4a437698b63f459d557c88"
 username = "dylrobinson22"
 
 def getSP():
@@ -57,37 +57,49 @@ def addFeatures(f, playlist):
                 f.append(track)
             k = k + 1
 
-sp = getSP()
+# sp = getSP()
 
-songs_playlist = sp.user_playlist('dylrobinson22', '23LcX1S7f04C8homMmZ03Z')
+# songs_playlist = sp.user_playlist('dylrobinson22', '23LcX1S7f04C8homMmZ03Z')
 
-song_features = []
+# song_features = []
 
-addFeatures(song_features, songs_playlist)
+# addFeatures(song_features, songs_playlist)
 
-song_df = pd.DataFrame(song_features)
+song_df = pd.read_csv('clustered_songs.csv')
 print(song_df.head())
 
-features = ["danceability", "loudness", "valence",
-            "energy", "key", "tempo"]
+features = ["valence",
+            "energy"]
 
 n_clusters = 7
 
 kmeans = KMeans(n_clusters=n_clusters)
 kmeans.fit(song_df[features])
+spectral = SpectralClustering(n_clusters=n_clusters, affinity='nearest_neighbors')
+spectral.fit(song_df[features])
 
-print('K-means cluster centers: ')
-print(kmeans.cluster_centers_)
 
-print('K-means labels:')
-print(kmeans.labels_)
+# print('K-means cluster centers: ')
+# print(kmeans.cluster_centers_)
+
+# print('K-means labels:')
+# print(kmeans.labels_)
+
+
 
 cluster_groups = kmeans.labels_
+cluster_groups_2 = spectral.labels_
+# for i in range(len(song_features)):
+#     song_features[i]['cluster'] = cluster_groups[i]
 
-for i in range(len(song_features)):
-    song_features[i]['cluster'] = cluster_groups[i]
 
-songs_clustered_df = pd.DataFrame(song_features)
+
+# songs_clustered_df = pd.DataFrame(song_features)
+
+songs_clustered_df = song_df
+songs_clustered_df.drop('cluster', axis=1, inplace=True)
+songs_clustered_df['cluster'] = cluster_groups
+songs_clustered_df['cluster2'] = cluster_groups_2
 
 songs_clustered_df.to_csv('clustered_songs2.csv', index=False)
 
@@ -98,3 +110,10 @@ sns.set_theme()
 for feature in features:
     sns.histplot(data=songs_clustered_df, x=feature, hue='cluster', bins=30, multiple='stack', palette=sns.color_palette("husl", n_clusters))
     plt.show()
+    sns.histplot(data=songs_clustered_df, x=feature, hue='cluster2', bins=30, multiple='stack', palette=sns.color_palette("husl", n_clusters))
+    plt.show()
+
+sns.scatterplot(data=songs_clustered_df, x='valence', y='energy', hue='cluster', palette=sns.color_palette("husl", n_clusters))
+plt.show()
+sns.scatterplot(data=songs_clustered_df, x='valence', y='energy', hue='cluster2', palette=sns.color_palette("husl", n_clusters))
+plt.show()
